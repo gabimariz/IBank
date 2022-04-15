a010fcb54734e45a41466d6a4f1b8687058bff2a
Gabriel Mariz <gabriellucena997@outlook.com>
2022-04-15 20:12:52 -0300

    signIn and GetById added for repository

--- Domain/Interfaces/IUserRepository.cs
+++ Domain/Interfaces/IUserRepository.cs
@@ -4,6 +4,9 @@ namespace Domain.Interfaces;
 
 public interface IUserRepository : IDisposable
 {
+	User SignIn(string email, string password);
+
+	User GetById(Guid id);
 	string GetByCpf(string cpf);
 
 	void Insert(User user);
