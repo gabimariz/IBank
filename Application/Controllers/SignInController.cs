60a8e94f89d601f3d57edb3c162685b96c8dea16
Gabriel Mariz <gabriellucena997@outlook.com>
2022-04-15 20:07:42 -0300

    login added

---
+++ Application/Controllers/SignInController.cs
@@ -0,0 +1,36 @@
+using Application.InputModels;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers;
+
+[ApiController]
+[Route("v1/[controller]")]
+public class SignInController : ControllerBase
+{
+	private readonly ISignInService _signInService;
+	private readonly ITokenService _tokenService;
+
+	public SignInController(ISignInService signInService, ITokenService tokenService)
+	{
+		_signInService = signInService;
+		_tokenService = tokenService;
+	}
+
+	[HttpPost]
+	public ActionResult<dynamic> AuthenticateAsync([FromBody] SignInInputModel signIn)
+	{
+		var user = _signInService.SignIn(signIn.Email!, signIn.Password!);
+
+		if (user == null)
+			return NotFound(new { message = "user not found!" });
+
+		var token = _tokenService.GenerateToken(user);
+
+		return new
+		{
+			user,
+			token
+		};
+	}
+}
