570b90e8b2c2370d642c11e9cbaf91c711315d22
Gabriel Mariz <gabriellucena997@outlook.com>
2022-04-16 17:08:33 -0300

    new: Pix transfer repository

---
+++ Infra/Repositories/PixRepository.cs
@@ -0,0 +1,30 @@
+using Domain.Entities;
+using Domain.Interfaces;
+using Infra.Data;
+
+namespace Infra.Repositories;
+
+public class PixRepository : IPixRepository
+{
+	private readonly AppDbContext _appDbContext;
+
+	public PixRepository(AppDbContext appDbContext)
+	{
+		_appDbContext = appDbContext;
+	}
+
+	public void Transfer(double money, Guid to, Guid from)
+	{
+		_appDbContext.PixTransfers!.Add(new PixTransfer
+		{
+			Money = money,
+			To = to,
+			From = from
+		});
+	}
+
+	public void Save()
+	{
+		_appDbContext.SaveChanges();
+	}
+}
