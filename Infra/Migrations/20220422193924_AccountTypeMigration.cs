3ac1730aadd753cb6984eb44edffb5c12fb44ae8
Gabriel Mariz <gabriellucena997@outlook.com>
2022-04-22 16:44:40 -0300

    update: migrations update account type

---
+++ Infra/Migrations/20220422193924_AccountTypeMigration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Migrations
+{
+    public partial class AccountTypeMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Account",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Account");
+        }
+    }
+}
