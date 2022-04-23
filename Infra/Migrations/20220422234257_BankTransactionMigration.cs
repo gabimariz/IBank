5a9de9b1b841b0c0d419e979835f18c135512bff
Gabriel Mariz <gabriellucena997@outlook.com>
2022-04-22 21:07:50 -0300

    update: new table update migrations

---
+++ Infra/Migrations/20220422234257_BankTransactionMigration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Migrations
+{
+    public partial class BankTransactionMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BankTransactions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Money = table.Column<double>(type: "double", nullable: false),
+                    To = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    From = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    TransactionType = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BankTransactions", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BankTransactions");
+        }
+    }
+}
