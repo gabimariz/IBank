ead6cde3e5868b6dc458074896ef9c2a5d30f426
Gabriel Mariz <gabriellucena997@outlook.com>
2022-04-26 18:44:30 -0300

    update: ignore values auto generate

--- Domain/Entities/Card.cs
+++ Domain/Entities/Card.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 using Domain.Enums;
 
 namespace Domain.Entities;
@@ -7,10 +8,13 @@ namespace Domain.Entities;
 public class Card
 {
 	[Key]
+	[JsonIgnore]
 	public Guid Id { get; set; }
 
+	[JsonIgnore]
 	public string? Number { get; set; }
 
+	[JsonIgnore]
 	public int Cvv { get; set; }
 
 	public string? Password { get; set; }
@@ -18,9 +22,11 @@ public class Card
 	[ForeignKey("User")]
 	public Guid UserId { get; set; }
 
+	[JsonIgnore]
 	public virtual User? User { get; set; }
 
 	public CardType Type { get; set; }
 
+	[JsonIgnore]
 	public DateTime Validity { get; set; }
 }
