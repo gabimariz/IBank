b3834adebaa539fdc981e3a8ebea59be435c0c1c
Gabriel Mariz <gabriellucena997@outlook.com>
2022-04-26 10:26:10 -0300

    new: card entity

---
+++ Domain/Entities/Card.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Domain.Enums;
+
+namespace Domain.Entities;
+
+public class Card
+{
+	[Key]
+	public Guid Id { get; set; }
+
+	public string? Number { get; set; }
+
+	public int Cvv { get; set; }
+
+	public string? Password { get; set; }
+
+	[ForeignKey("User")]
+	public Guid UserId { get; set; }
+
+	public virtual User? User { get; set; }
+
+	public CardType Type { get; set; }
+}
