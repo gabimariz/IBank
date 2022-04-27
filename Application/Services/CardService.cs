2b478001550d240e0fc0e60539eb73dd4b8e1320
Gabriel Mariz <gabriellucena997@outlook.com>
2022-04-27 10:17:34 -0300

    new: getby card service

--- Application/Services/CardService.cs
+++ Application/Services/CardService.cs
@@ -1,16 +1,28 @@
+using Application.InputModels;
 using Domain.Entities;
+using Domain.Exceptions;
 using Domain.Interfaces;
 using Domain.Utils;
 
 namespace Application.Services;
 
-public class CardService : ICardService
+public class CardService : ICardService<GetCardByUserIdInputModel>
 {
 	private readonly ICardRepository _cardRepository;
 
 	public CardService(ICardRepository cardRepository)
 	{
 		_cardRepository = cardRepository;
+	}
+
+	public Card GetByCardNumber(GetCardByUserIdInputModel input)
+	{
+		var card = _cardRepository.GetByCardNumber(input.UserId);
+
+		if (card == null)
+			throw new UnlinkedCardException();
+
+		return card;
 	}
 
 	public Card Create(Card card)
